05975f7e2f1065a02cf69b85743109ca606c440b
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-07 21:34:59 +0100

    plugings Vuetify

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -5,8 +5,6 @@ import 'vuetify/styles'
 // Vuetify
 import { createVuetify } from 'vuetify'
 
-export default createVuetify({
-  theme: {
-    defaultTheme: 'light'
-  }
-})
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
