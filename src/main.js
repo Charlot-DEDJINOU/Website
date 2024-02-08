0079d064298c74463e0cd0d9f8159d697c6ec38c
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-08 23:29:38 +0100

    Configure I18n in my app

--- src/main.js
+++ src/main.js
@@ -7,9 +7,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { createI18n } from 'vue-i18n'
 
-const app = createApp(App).use(store)
+// Importez vos fichiers de messages pour chaque langue
+import frMessages from './translate/fr.json'
+import enMessages from './translate/en.json'
 
+const i18n = createI18n({
+  locale: 'fr',
+  fallbackLocale: 'en', 
+  messages: {
+    fr: frMessages,
+    en: enMessages
+  }
+})
+
+const app = createApp(App)
 app.use(router)
+app.use(store)
+app.use(i18n)
 
 app.mount('#app')
