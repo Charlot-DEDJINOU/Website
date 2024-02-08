690bfbf9b95fc4af1e2d83442a76cab0fe76c2e9
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-08 23:44:58 +0100

    testing i18n

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,12 @@ import { RouterView } from 'vue-router'
 </script>
 
 <template>
+  <p>{{ $t('hello') }}</p>
+  <div class="locale-changer">
+    <select v-model="$i18n.locale">
+      <option v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`" :value="locale">{{ locale }}</option>
+    </select>
+  </div>
   <RouterView />
 </template>
 
