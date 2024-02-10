f28f3c6ffb716d59721f780b9c8a3ec888410f77
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-10 23:10:06 +0100

    CustomDrawer

---
+++ src/components/CustomDrawer.vue
@@ -0,0 +1,47 @@
+<script>
+import SocialMedia from './SocialMedia.vue'
+import DrawerOnglets from './DrawerOnglets.vue'
+import { useStore } from 'vuex'
+import { computed, ref } from 'vue'
+
+export default {
+  components: {
+    SocialMedia,
+    DrawerOnglets
+  },
+  setup() {
+    const store = useStore()
+    const uniColor = ref(computed(() => store.state.uniColor))
+
+    return {
+      uniColor
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="drawer p-3 d-flex flex-column align-items-center">
+    <div class="drawer-header d-flex align-items-center">
+      <img src="../assets/charlot.jpg" alt="charlot" :style="{ border: '3px solid' + uniColor }" />
+      <span class="d-inline-block mx-3 fw-medium">Charlot Joël DEDJINOU</span>
+    </div>
+    <div class="my-4 w-100 d-flex flex-column align-items-center">
+      <SocialMedia />
+      <DrawerOnglets />
+    </div>
+  </div>
+</template>
+
+<style>
+.drawer {
+  width: 100%;
+  height: auto;
+  z-index: 10;
+}
+.drawer .drawer-header img {
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+}
+</style>
