f559285adf0270242e7da56158b71e115e94f682
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-11 19:47:20 +0100

    Section Title

---
+++ src/components/SectionTitle.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  props: {
+    title: String
+  }
+}
+</script>
+<template>
+  <div class="section-title d-flex flex-column align-items-center">
+    <h2>{{ title }}</h2>
+    <span class="d-inline-block"></span>
+  </div>
+</template>
+
+<style scoped>
+.section-title span {
+  width: 25px;
+  height: 5px;
+  background-color: green;
+  border-radius: 5px;
+  animation: translateAnimation 2s infinite;
+}
+
+@keyframes translateAnimation {
+  0% {
+    transform: translateX(-20px);
+  }
+  50% {
+    transform: translateX(20px);
+  }
+  100% {
+    transform: translateX(-20px);
+  }
+}
+</style>
