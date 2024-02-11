a2db9c9ed979b2f03d5152c86d30e354af708efb
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-11 12:51:51 +0100

    Home Section

---
+++ src/components/HomeSection.vue
@@ -0,0 +1,134 @@
+<script>
+import CustomHeader from './CustomHeader.vue'
+import ThemesColors from './ThemesColors.vue'
+import IconSetting from './icons/IconSetting.vue'
+import PresentationProfil from './PresentationProfil.vue'
+import IconArrowDown from './icons/IconArrowDown.vue';
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+
+export default {
+  components: {
+    CustomHeader,
+    ThemesColors,
+    IconSetting,
+    PresentationProfil,
+    IconArrowDown
+},
+  setup() {
+    const store = useStore()
+    const uniColor = ref(computed(() => store.state.uniColor))
+    const theme = ref(computed(() => store.state.theme))
+
+    const show = ref(false)
+    const handdleShow = () => (show.value = !show.value)
+
+    return {
+      show,
+      handdleShow,
+      uniColor,
+      theme
+    }
+  }
+}
+</script>
+<template>
+  <section
+    class="presentation d-flex flex-column"
+    :style="{ backgroundImage: 'url(' + theme.background.image + ')' }"
+  >
+    <CustomHeader />
+    <div class="home-preference d-flex align-items-center">
+      <span
+        class="d-inline-block d-flex justify-content-center align-items-center mx-3 py-1 px-2 show-colors"
+        @click="handdleShow()"
+        :style="{ backgroundColor: uniColor }"
+      >
+        <IconSetting size="15" class="rotate text-white" />
+      </span>
+      <ThemesColors class="opacity" v-if="show" />
+    </div>
+    <div class="w-100 h-100 d-flex flex-column align-items-center justify-content-evenly" :style="{marginTop : '80px'}" >
+      <PresentationProfil :color="uniColor" :theme="theme" />
+      <IconArrowDown class="scroll-about" :style="{color : uniColor}"/>
+    </div>
+  </section>
+</template>
+<style scoped>
+.presentation {
+  width: 100%;
+  height: 100vh;
+  background-size: cover;
+  background-position: center;
+  padding-bottom: 15px;
+}
+.presentation .home-preference {
+  position: absolute;
+  top: 30%;
+  cursor: pointer;
+  height: 150px;
+}
+.presentation .home-preference .show-colors {
+  border-radius: 3px;
+}
+.presentation .home-preference .show-colors .rotate {
+  transform-origin: center;
+  animation-name: rotate;
+  animation-duration: 2s;
+  animation-iteration-count: infinite;
+  animation-timing-function: linear;
+}
+.presentation .home-preference .opacity {
+  opacity: 1;
+  transition: opacity 2s ease-in-out;
+  transform: translateY(20px);
+}
+.presentation .scroll-about {
+  width: 60px;
+  height: 60px;
+  margin-bottom: -50px;
+  animation: scrollAnimation 1s infinite;
+}
+@keyframes scrollAnimation {
+  0% {
+      transform: translateY(0);
+      box-shadow: none;
+  }
+  50% {
+      transform: translateY(10px);
+  }
+  100% {
+      transform: translateY(0);
+      box-shadow: none;
+  }
+}
+@keyframes rotate {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+@media screen and (max-width: 768px) {
+  .presentation {
+    height: 100vh;
+    padding-bottom: 30px;
+  }
+  .presentation .scroll-about {
+    margin-bottom: 0px;
+    margin-top: 50px;
+  }
+}
+@media screen and (max-width: 500px) {
+  .presentation {
+    min-height: 100vh;
+    height: auto;
+    padding-bottom: 30px;
+  }
+}
+@media screen and (min-width: 769px) and (max-width: 1024px) {
+}
+@media screen and (min-width: 1025px) {
+}
+</style>
