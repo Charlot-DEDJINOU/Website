7117e26171801d79473ec2f23f67f93f0b31f28d
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-12 21:29:31 +0100

    add homesection

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,15 @@
 <script setup>
-import CustomHeader from '../components/CustomHeader.vue'
+import HomeSection from '../components/HomeSection.vue'
 import AboutSection from '../components/AboutSection.vue'
+import EducationExperienceSection from '../components/EducationExperienceSection.vue'
+import SkillsSection from '../components/SkillsSection.vue'
 </script>
 
 <template>
   <main>
-    <CustomHeader />
+    <HomeSection />
     <AboutSection />
+    <EducationExperienceSection />
+    <SkillsSection />
   </main>
 </template>
