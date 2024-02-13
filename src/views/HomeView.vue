72f9f53969cdc41149977e1f71aa4941ad82ed2d
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-13 21:19:18 +0100

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,19 @@
 <script setup>
-import CustomHeader from '../components/CustomHeader.vue'
+import HomeSection from '../components/HomeSection.vue'
+import AboutSection from '../components/AboutSection.vue'
+import EducationExperienceSection from '../components/EducationExperienceSection.vue'
+import SkillsSection from '../components/SkillsSection.vue'
 import ServicesSection from '../components/ServicesSection.vue'
+import ProjectsSectionVue from '../components/ProjectsSection.vue'
 </script>
 
 <template>
   <main>
-    <CustomHeader />
+    <HomeSection />
+    <AboutSection />
+    <EducationExperienceSection />
+    <SkillsSection />
     <ServicesSection />
+    <ProjectsSectionVue />
   </main>
 </template>
