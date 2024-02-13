c29038437e253541949e3599adfc62fb4b8c439b
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-13 21:20:34 +0100

    Uodate

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,24 +1,24 @@
 <script>
 import SectionTitle from './SectionTitle.vue'
-import ServiceItem from './ServiceItem.vue';
+import ServiceItem from './ServiceItem.vue'
 
 export default {
   components: {
     SectionTitle,
     ServiceItem
-}
+  }
 }
 </script>
 <template>
   <section class="mt-5">
     <SectionTitle title="Services" />
     <div class="container d-flex flex-wrap justify-content-around mt-5">
-        <ServiceItem />
-        <ServiceItem />
-        <ServiceItem />
-        <ServiceItem />
-        <ServiceItem />
-        <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
+      <ServiceItem />
     </div>
   </section>
 </template>
