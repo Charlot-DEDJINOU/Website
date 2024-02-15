79caa8f21ffcc138791bc2cbc4d1f1377ae6f857
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-15 20:49:11 +0100

    Awards Section

---
+++ src/components/AwardsSection.vue
@@ -0,0 +1,35 @@
+<script>
+import SectionTitle from './SectionTitle.vue'
+import { ref, computed } from 'vue'
+import { useStore } from 'vuex'
+import AwardItem from './AwardItem.vue'
+
+export default {
+  components: {
+    SectionTitle,
+    AwardItem
+  },
+  setup() {
+    const store = useStore()
+
+    const uniColor = ref(computed(() => store.state.uniColor))
+    const theme = ref(computed(() => store.state.theme))
+
+    return {
+      uniColor,
+      theme
+    }
+  }
+}
+</script>
+<template>
+  <section class="mt-5 projects">
+    <SectionTitle title="Distinctions" />
+    <div class="container d-flex flex-wrap justify-content-around mt-5">
+      <AwardItem />
+      <AwardItem />
+      <AwardItem />
+    </div>
+  </section>
+</template>
+<style scoped></style>
