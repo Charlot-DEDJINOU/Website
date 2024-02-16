18e46343249d989c9a45030a010b58496ca52a3b
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-16 22:56:25 +0100

    Contact Section

---
+++ src/components/ContactSection.vue
@@ -0,0 +1,20 @@
+<script>
+import SectionTitle from './SectionTitle.vue'
+import SocialMedia from './SocialMedia.vue'
+
+export default {
+  components: {
+    SectionTitle,
+    SocialMedia
+  }
+}
+</script>
+<template>
+  <section class="mt-5 contact" id="contact">
+    <SectionTitle title="Contact" />
+    <div class="container d-flex flex-wrap justify-content-around mt-5">
+      <SocialMedia iconSize="50" size="50%" />
+    </div>
+  </section>
+</template>
+<style scoped></style>
