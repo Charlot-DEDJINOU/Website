3c22a8d0ab1a86dae17d78da44fc4bd396857ffb
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-18 23:39:02 +0100

    My Services

---
+++ src/data/Services.js
@@ -0,0 +1,40 @@
+export default function Services() {
+  return [
+    {
+      icon: 0,
+      title: 'Développement Web',
+      description:
+        'Créez des sites web dynamiques et interactifs, offrant une expérience utilisateur exceptionnelle et des fonctionnalités personnalisées.'
+    },
+    {
+      icon: 1,
+      title: 'Applications Mobiles',
+      description:
+        'Concevez des applications mobiles performantes pour iOS et Android, offrant des interfaces utilisateur élégantes et des fonctionnalités innovantes.'
+    },
+    {
+      icon: 2,
+      title: "Développement d'API",
+      description:
+        "Concevez, développez et déployez des API robustes pour permettre l'intégration et l'interaction entre différentes applications et systèmes."
+    },
+    {
+      icon: 3,
+      title: 'Analyse de donnée',
+      description:
+        'Analysez des ensembles de données complexes pour extraire des insights significatifs et construire des modèles prédictifs.'
+    },
+    {
+      icon: 4,
+      title: "Génération de lien J'y serai",
+      description:
+        "Créez un système pour les organisateurs d'événements afin de générer des liens J'y serai personnalisés pour la promotion de leurs événements."
+    },
+    {
+      icon: 5,
+      title: 'Interfaces Utilisateur',
+      description:
+        'Créez des interfaces intuitives et esthétiques pour les sites web et les applications mobiles ainsi que des visuels pour des évenements.'
+    }
+  ]
+}
