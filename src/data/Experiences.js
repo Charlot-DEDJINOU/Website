67b29911b6d804057c9b4007532a614eca7ff182
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-18 23:38:42 +0100

    My Expereinces

---
+++ src/data/Experiences.js
@@ -0,0 +1,25 @@
+export default function EducationData() {
+  return [
+    {
+      title: 'Développeur Web  Frontend',
+      years: 'Août 2023 - Otobre 2023 ',
+      place: 'AGCS LIDAL, Abomey-Calavi, Bénin',
+      skills: [
+        'Fusion de trois sites WordPress en une plateforme unifiée.',
+        'Utilisation de React JS (frontend)',
+        'Résolution de problèmes techniques et débogage'
+      ]
+    },
+    {
+      title: 'Developpeur Full-Stack',
+      years: 'Janvier 2023 - Mars 2023',
+      place: 'Hackaton SENE, Direction du numérique, Bénin',
+      skills: [
+        'Modélisation du site en utilisant le language UML',
+        'Réalisation de la maquette avec le logiciel Figma',
+        'Implementation du front-end utilisant React Js',
+        'Développement de l’API du site en utilisant Laravel'
+      ]
+    }
+  ]
+}
