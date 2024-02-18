6ff29c1d0d0414fba255db0b482dd6e0e2334bef
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-18 23:39:19 +0100

    My Skills

---
+++ src/data/Skills.js
@@ -0,0 +1,56 @@
+export default function Skills() {
+  return [
+    [
+      {
+        title: 'HTML, CSS, JS, PHP',
+        percent: '95%'
+      },
+      {
+        title: 'Python, Data Science',
+        percent: '85%'
+      },
+      {
+        title: 'Bootstrap, React JS, Vue JS',
+        percent: '90%'
+      },
+      {
+        title: 'Laravel, Express JS, Fast API',
+        percent: '90%'
+      },
+      {
+        title: 'Dart, Flutter, React Native',
+        percent: '85%'
+      },
+      {
+        title: 'WebSocket',
+        percent: '75%'
+      }
+    ],
+    [
+      {
+        title: 'MySql, Oracle, Sql',
+        percent: '85%'
+      },
+      {
+        title: 'Java, Spring Boot',
+        percent: '75%'
+      },
+      {
+        title: 'Cryptographie, IOT',
+        percent: '60%'
+      },
+      {
+        title: 'Figma, Adobe XD, Illustrator',
+        percent: '85%'
+      },
+      {
+        title: 'Fluentd, Opensearch, Opensearch Dashboard',
+        percent: '70%'
+      },
+      {
+        title: 'Git, Github, C, C++',
+        percent: '90%'
+      }
+    ]
+  ]
+}
