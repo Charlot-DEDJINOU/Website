b77c8ca43cfd37dcbd0305f4d89f4af329d05ee7
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-19 19:37:35 +0100

    dynamic section services

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -1,11 +1,19 @@
 <script>
 import SectionTitle from './SectionTitle.vue'
 import ServiceItem from './ServiceItem.vue'
+import Services from '../data/Services'
 
 export default {
   components: {
     SectionTitle,
     ServiceItem
+  },
+  setup() {
+    const services = Services()
+
+    return {
+      services
+    }
   }
 }
 </script>
@@ -13,12 +21,12 @@ export default {
   <section class="mt-3" id="services">
     <SectionTitle title="Services" />
     <div class="container d-flex flex-wrap justify-content-between mt-3">
-      <ServiceItem />
-      <ServiceItem />
-      <ServiceItem />
-      <ServiceItem />
-      <ServiceItem />
-      <ServiceItem />
+      <ServiceItem
+        v-for="(item, index) in services"
+        :key="index"
+        :title="item.title"
+        :content="item.description"
+      />
     </div>
   </section>
 </template>
