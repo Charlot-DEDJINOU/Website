44dad081f0938b6e0f3784ff2da2f089c0865b8c
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-24 23:42:11 +0100

    data certifications

---
+++ src/data/Certifications.js
@@ -0,0 +1,245 @@
+import cert_1 from '../assets/cert_1.png'
+import cert_2 from '../assets/cert_2.png'
+import cert_3 from '../assets/cert_3.png'
+import cert_4 from '../assets/cert_4.png'
+import cert_5 from '../assets/cert_5.png'
+import cert_6 from '../assets/cert_6.png'
+import cert_7 from '../assets/cert_7.png'
+import cert_8 from '../assets/cert_8.png'
+import cert_9 from '../assets/cert_9.png'
+import cert_10 from '../assets/cert_10.png'
+import cert_11 from '../assets/cert_11.png'
+import cert_12 from '../assets/cert_12.png'
+import cert_13 from '../assets/cert_13.png'
+import cert_14 from '../assets/cert_14.png'
+import cert_15 from '../assets/cert_15.png'
+import cert_16 from '../assets/cert_16.png'
+import cert_17 from '../assets/cert_17.png'
+import cert_18 from '../assets/cert_18.png'
+import cert_19 from '../assets/cert_19.png'
+import cert_20 from '../assets/cert_20.png'
+import cert_21 from '../assets/cert_21.png'
+import cert_22 from '../assets/cert_22.png'
+import cert_23 from '../assets/cert_23.png'
+import cert_24 from '../assets/cert_24.png'
+import cert_25 from '../assets/cert_25.png'
+import cert_26 from '../assets/cert_26.png'
+import cert_27 from '../assets/cert_27.png'
+import cert_28 from '../assets/cert_28.png'
+import cert_30 from '../assets/cert_30.png'
+import cert_31 from '../assets/cert_31.png'
+import cert_32 from '../assets/cert_32.png'
+import cert_33 from '../assets/cert_33.png'
+import cert_34 from '../assets/cert_34.png'
+import cert_35 from '../assets/cert_35.png'
+import cert_36 from '../assets/cert_36.png'
+import cert_37 from '../assets/cert_37.png'
+import cert_38 from '../assets/cert_38.png'
+import cert_39 from '../assets/cert_39.png'
+import cert_40 from '../assets/cert_40.png'
+import cert_41 from '../assets/cert_41.png'
+import cert_42 from '../assets/cert_42.png'
+import cert_43 from '../assets/cert_43.png'
+import cert_44 from '../assets/cert_44.png'
+import cert_45 from '../assets/cert_45.png'
+import cert_46 from '../assets/cert_46.png'
+import cert_47 from '../assets/cert_47.png'
+import cert_48 from '../assets/cert_48.png'
+import cert_49 from '../assets/cert_49.png'
+
+export default function Certifications() {
+  return [
+    {
+      image: cert_1,
+      name: 'Initiez-vous au machine learning'
+    },
+    {
+      image: cert_2,
+      name: "Evaluez les performances d'un modele de machine learning"
+    },
+    {
+      image: cert_3,
+      name: 'Entrainez un modele predictif lineaire'
+    },
+    {
+      image: cert_4,
+      name: 'Explorez le paronama des objects connectés'
+    },
+    {
+      image: cert_5,
+      name: 'Decouvrez les librairies python pour la data science'
+    },
+    {
+      image: cert_6,
+      name: 'Realisez une analyse exploratoire de donnée'
+    },
+    {
+      image: cert_7,
+      name: 'Maitrisez les bases des probabilités'
+    },
+    {
+      image: cert_8,
+      name: 'Nettoyez et analysez votre jeu de donnée'
+    },
+    {
+      image: cert_9,
+      name: "Objectif IA : initiez-vous à l'intelligence artificielle"
+    },
+    {
+      image: cert_10,
+      name: 'Python data structures'
+    },
+    {
+      image: cert_11,
+      name: "Initiez-vous à python pour l'analyse de données"
+    },
+    {
+      image: cert_12,
+      name: 'Apprenez les bases du language python'
+    },
+    {
+      image: cert_13,
+      name: 'Apprenez la programmation orientée objet avec python'
+    },
+    {
+      image: cert_14,
+      name: 'Créez des pages web dynamiques avec javascript'
+    },
+    {
+      image: cert_15,
+      name: 'Ecrivez du javascript pour le web'
+    },
+    {
+      image: cert_16,
+      name: 'Créez une application React complete'
+    },
+    {
+      image: cert_17,
+      name: 'Débutez avec React'
+    },
+    {
+      image: cert_18,
+      name: 'React + Redux'
+    },
+    {
+      image: cert_19,
+      name: 'Créez une application web avec vue.js'
+    },
+    {
+      image: cert_20,
+      name: 'Devenez developpeur agile'
+    },
+    {
+      image: cert_21,
+      name: 'PHP'
+    },
+    {
+      image: cert_22,
+      name: 'SQL'
+    },
+    {
+      image: cert_23,
+      name: 'Implementez vos bases de donnees relationnelles avec SQL'
+    },
+    {
+      image: cert_24,
+      name: 'Concevez votre site web avec php et mysql'
+    },
+    {
+      image: cert_25,
+      name: 'Gerez des visuels avec illustrator'
+    },
+    {
+      image: cert_26,
+      name: 'Créez votre site web avec HTML5 et CSS3'
+    },
+    {
+      image: cert_27,
+      name: 'Apprenez à programmer avec javascript'
+    },
+    {
+      image: cert_28,
+      name: 'javascript'
+    },
+    {
+      image: cert_30,
+      name: "Ameliorer l'impact de vos presentations"
+    },
+    {
+      image: cert_31,
+      name: 'Debutez avec le framework django'
+    },
+    {
+      image: cert_32,
+      name: 'Apprenez à programmer en C++'
+    },
+    {
+      image: cert_33,
+      name: 'Python for beginners'
+    },
+    {
+      image: cert_34,
+      name: 'html'
+    },
+    {
+      image: cert_35,
+      name: 'kotlin'
+    },
+    {
+      image: cert_36,
+      name: 'css'
+    },
+    {
+      image: cert_37,
+      name: "Realisez la maquette d'une application mobile avec adobe xd"
+    },
+    {
+      image: cert_38,
+      name: 'Gérez du code avec git et github'
+    },
+    {
+      image: cert_39,
+      name: 'Apprenez à coder en java'
+    },
+    {
+      image: cert_40,
+      name: 'Installer votre environnement de developpement java avec eclipse'
+    },
+    {
+      image: cert_41,
+      name: 'Apprenez à utiliser la ligne de commande dans un terminal'
+    },
+    {
+      image: cert_42,
+      name: 'Prenez la parole en public'
+    },
+    {
+      image: cert_43,
+      name: 'Gérez votre temps efficacement'
+    },
+    {
+      image: cert_44,
+      name: 'Créez une application avec spring boot'
+    },
+    {
+      image: cert_45,
+      name: 'Mettez en place un système de veille informationnelle'
+    },
+    {
+      image: cert_46,
+      name: 'Rédigez des ecrits professionnels'
+    },
+    {
+      image: cert_47,
+      name: 'Travaillez efficacement en equipe'
+    },
+    {
+      image: cert_48,
+      name: 'Initiez-vous à linux'
+    },
+    {
+      image: cert_49,
+      name: 'Apprenez à apprendre'
+    }
+  ]
+}
