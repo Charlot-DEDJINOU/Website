7863997b1d2822af9914adaecbf03c8bc4e441ac
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-26 18:44:44 +0100

    add id

--- src/data/Certifications.js
+++ src/data/Certifications.js
@@ -50,194 +50,242 @@ import cert_49 from '../assets/cert_49.png'
 export default function Certifications() {
   return [
     {
+      id: 0,
       image: cert_1,
       name: 'Initiez-vous au machine learning'
     },
     {
+      id: 1,
       image: cert_2,
       name: "Evaluez les performances d'un modele de machine learning"
     },
     {
+      id: 2,
       image: cert_3,
       name: 'Entrainez un modele predictif lineaire'
     },
     {
+      id: 3,
       image: cert_4,
       name: 'Explorez le paronama des objects connectés'
     },
     {
+      id: 4,
       image: cert_5,
       name: 'Decouvrez les librairies python pour la data science'
     },
     {
+      id: 5,
       image: cert_6,
       name: 'Realisez une analyse exploratoire de donnée'
     },
     {
+      id: 6,
       image: cert_7,
       name: 'Maitrisez les bases des probabilités'
     },
     {
+      id: 7,
       image: cert_8,
       name: 'Nettoyez et analysez votre jeu de donnée'
     },
     {
+      id: 8,
       image: cert_9,
       name: "Objectif IA : initiez-vous à l'intelligence artificielle"
     },
     {
+      id: 9,
       image: cert_10,
       name: 'Python data structures'
     },
     {
+      id: 10,
       image: cert_11,
       name: "Initiez-vous à python pour l'analyse de données"
     },
     {
+      id: 11,
       image: cert_12,
       name: 'Apprenez les bases du language python'
     },
     {
+      id: 12,
       image: cert_13,
       name: 'Apprenez la programmation orientée objet avec python'
     },
     {
+      id: 13,
       image: cert_14,
       name: 'Créez des pages web dynamiques avec javascript'
     },
     {
+      id: 14,
       image: cert_15,
       name: 'Ecrivez du javascript pour le web'
     },
     {
+      id: 15,
       image: cert_16,
       name: 'Créez une application React complete'
     },
     {
+      id: 16,
       image: cert_17,
       name: 'Débutez avec React'
     },
     {
+      id: 17,
       image: cert_18,
       name: 'React + Redux'
     },
     {
+      id: 18,
       image: cert_19,
       name: 'Créez une application web avec vue.js'
     },
     {
+      id: 19,
       image: cert_20,
       name: 'Devenez developpeur agile'
     },
     {
+      id: 20,
       image: cert_21,
       name: 'PHP'
     },
     {
+      id: 21,
       image: cert_22,
       name: 'SQL'
     },
     {
+      id: 22,
       image: cert_23,
       name: 'Implementez vos bases de donnees relationnelles avec SQL'
     },
     {
+      id: 23,
       image: cert_24,
       name: 'Concevez votre site web avec php et mysql'
     },
     {
+      id: 24,
       image: cert_25,
       name: 'Gerez des visuels avec illustrator'
     },
     {
+      id: 25,
       image: cert_26,
       name: 'Créez votre site web avec HTML5 et CSS3'
     },
     {
+      id: 26,
       image: cert_27,
       name: 'Apprenez à programmer avec javascript'
     },
     {
+      id: 27,
       image: cert_28,
       name: 'javascript'
     },
     {
+      id: 28,
       image: cert_30,
       name: "Ameliorer l'impact de vos presentations"
     },
     {
+      id: 29,
       image: cert_31,
       name: 'Debutez avec le framework django'
     },
     {
+      id: 30,
       image: cert_32,
       name: 'Apprenez à programmer en C++'
     },
     {
+      id: 31,
       image: cert_33,
       name: 'Python for beginners'
     },
     {
+      id: 32,
       image: cert_34,
       name: 'html'
     },
     {
+      id: 33,
       image: cert_35,
       name: 'kotlin'
     },
     {
+      id: 34,
       image: cert_36,
       name: 'css'
     },
     {
+      id: 35,
       image: cert_37,
       name: "Realisez la maquette d'une application mobile avec adobe xd"
     },
     {
+      id: 36,
       image: cert_38,
       name: 'Gérez du code avec git et github'
     },
     {
+      id: 37,
       image: cert_39,
       name: 'Apprenez à coder en java'
     },
     {
+      id: 38,
       image: cert_40,
       name: 'Installer votre environnement de developpement java avec eclipse'
     },
     {
+      id: 39,
       image: cert_41,
       name: 'Apprenez à utiliser la ligne de commande dans un terminal'
     },
     {
+      id: 40,
       image: cert_42,
       name: 'Prenez la parole en public'
     },
     {
+      id: 41,
       image: cert_43,
       name: 'Gérez votre temps efficacement'
     },
     {
+      id: 42,
       image: cert_44,
       name: 'Créez une application avec spring boot'
     },
     {
+      id: 43,
       image: cert_45,
       name: 'Mettez en place un système de veille informationnelle'
     },
     {
+      id: 44,
       image: cert_46,
       name: 'Rédigez des ecrits professionnels'
     },
     {
+      id: 45,
       image: cert_47,
       name: 'Travaillez efficacement en equipe'
     },
     {
+      id: 46,
       image: cert_48,
       name: 'Initiez-vous à linux'
     },
     {
+      id: 47,
       image: cert_49,
       name: 'Apprenez à apprendre'
     }
