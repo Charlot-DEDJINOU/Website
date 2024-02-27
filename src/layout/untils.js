832842d3676817a0c6f02654bc7e658720c1725d
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-27 12:24:51 +0100

    downloadCV, viewCV and toggleMenu function

---
+++ src/layout/untils.js
@@ -0,0 +1,23 @@
+import pdfPath from '../data/Charlot_DEDJINOU_CV.pdf'
+
+export function downloadCV() {
+  const link = document.createElement('a')
+  link.download = 'Charlot_DEDJINOU_CV.pdf'
+  link.href = pdfPath
+
+  link.click()
+}
+
+export function viewCV() {
+  window.open(pdfPath, '_blank')
+}
+
+export function toggleMenu(url) {
+  const btn = document.getElementsByClassName('btn-close')[0]
+
+  const link = document.createElement('a')
+  link.href = url
+
+  btn.click()
+  link.click()
+}
