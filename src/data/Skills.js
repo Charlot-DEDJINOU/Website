fad75af4681e72613efaca3734c12b50ce7345e5
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-02-29 23:01:10 +0100

    update percents

--- src/data/Skills.js
+++ src/data/Skills.js
@@ -2,24 +2,24 @@ export default function Skills() {
   return [
     [
       {
-        title: 'HTML, CSS, JS, PHP',
-        percent: '95%'
+        title: 'HTML, CSS, JS, PHP,Python',
+        percent: '90%'
       },
       {
-        title: 'Python, Data Science',
-        percent: '85%'
+        title: 'Data Science, Machine Learning',
+        percent: '60%'
       },
       {
         title: 'Bootstrap, React JS, Vue JS',
-        percent: '90%'
+        percent: '85%'
       },
       {
         title: 'Laravel, Express JS, Fast API',
-        percent: '90%'
+        percent: '85%'
       },
       {
         title: 'Dart, Flutter, React Native',
-        percent: '85%'
+        percent: '80%'
       },
       {
         title: 'WebSocket',
@@ -29,27 +29,27 @@ export default function Skills() {
     [
       {
         title: 'MySql, Oracle, Sql',
-        percent: '85%'
-      },
-      {
-        title: 'Java, Spring Boot',
         percent: '75%'
       },
       {
-        title: 'Cryptographie, IOT',
-        percent: '60%'
-      },
-      {
-        title: 'Figma, Adobe XD, Illustrator',
-        percent: '85%'
-      },
-      {
-        title: 'Fluentd, Opensearch, Opensearch Dashboard',
+        title: 'Java, Spring Boot',
         percent: '70%'
       },
       {
+        title: 'Cryptographie, IOT',
+        percent: '50%'
+      },
+      {
+        title: 'Figma, Illustrator',
+        percent: '80%'
+      },
+      {
+        title: 'Fluentd, Opensearch, Opensearch Dashboard',
+        percent: '65%'
+      },
+      {
         title: 'Git, Github, C, C++',
-        percent: '90%'
+        percent: '85%'
       }
     ]
   ]
