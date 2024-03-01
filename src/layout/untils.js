e2af89cbb00ce83a99916c750972b6fb6afe23ba
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-01 23:58:08 +0100

    charging dynamic cv

--- src/layout/untils.js
+++ src/layout/untils.js
@@ -1,15 +1,16 @@
-import pdfPath from '../data/Charlot_DEDJINOU_CV.pdf'
+import pdfPathFr from '../data/Charlot_DEDJINOU_Fr.pdf'
+import pdfPathEn from '../data/Charlot_DEDJINOU_En.pdf'
 
-export function downloadCV() {
+export function downloadCV(locale) {
   const link = document.createElement('a')
   link.download = 'Charlot_DEDJINOU_CV.pdf'
-  link.href = pdfPath
+  link.href = locale === 'fr' ? pdfPathFr : pdfPathEn
 
   link.click()
 }
 
-export function viewCV() {
-  window.open(pdfPath, '_blank')
+export function viewCV(locale) {
+  window.open(locale === 'fr' ? pdfPathFr : pdfPathEn, '_blank')
 }
 
 export function toggleMenu(url) {
