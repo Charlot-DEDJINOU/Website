f388e80e9585f9bafe05a6ac4d6f5e61f6e09c52
Charlot-DEDJINOU <dedjinoucharlotjoel@gmail.com>
2024-03-22 16:01:35 +0100

    IOT Category

--- src/data/ProjectsCategories.js
+++ src/data/ProjectsCategories.js
@@ -3,5 +3,5 @@ import { useI18n } from 'vue-i18n'
 export default function ProjectsCategory() {
   const { t } = useI18n()
 
-  return [t('data_science'), t('web'), t('mobile'), t('programming'), t('websocket')]
+  return [t('iot'),t('data_science'), t('web'), t('mobile'), t('programming'), t('websocket')]
 }
