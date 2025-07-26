f1b5bf72770ac184dac08d1551f4e04b4add1578
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2025-07-26 22:48:38 +0100

    Modification de src/data/Educations.js

--- src/data/Educations.js
+++ src/data/Educations.js
@@ -8,28 +8,19 @@ export default function EducationData() {
       title: t('bachelor'),
       years: 'Oct 2021 - Mar 2025',
       place: t('ifri'),
-      skills: [
-        t('mention_tb_1614'),
-        t('software_engineering')
-      ]
+      skills: [t('mention_tb_1614'), t('software_engineering')]
     },
     {
       title: t('mastercard'),
       years: 'Oct 2021 - Oct 2024',
       place: t('university_abomey'),
-      skills: [
-        t('mastercard_scholar'),
-        t('cohort_leader')
-      ]
+      skills: [t('mastercard_scholar'), t('cohort_leader')]
     },
     {
       title: t('bachelor_science'),
       years: '2014 - 2021',
       place: t('cotonou'),
-      skills: [
-        t('mention_tb_1681'),
-        t('class_leader')
-      ]
+      skills: [t('mention_tb_1681'), t('class_leader')]
     }
   ]
 }
