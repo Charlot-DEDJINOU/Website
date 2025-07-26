9c57b296f91f3caba41d15f5a732e586fc098877
Charlot DEDJINOU <dedjinoucharlotjoel@gmail.com>
2025-07-26 22:48:39 +0100

    Modification de src/data/Experiences.js

--- src/data/Experiences.js
+++ src/data/Experiences.js
@@ -8,29 +8,19 @@ export default function EducationData() {
       title: t('developer_fullstack'),
       years: 'Jan 2023 - Jan 2025',
       place: 'ECOTIC, Abomey-Calavi, Benin',
-      skills: [
-        t('uml_modeling'),
-        t('react_redux_laravel'),
-        t('vps_deployment')
-      ]
+      skills: [t('uml_modeling'), t('react_redux_laravel'), t('vps_deployment')]
     },
     {
       title: t('developer_frontend'),
       years: 'Apr 2024 - Sep 2024',
       place: 'SPTIC - UAC, Abomey-Calavi, Benin',
-      skills: [
-        t('uac_portal_ui'),
-        t('api_integration')
-      ]
+      skills: [t('uac_portal_ui'), t('api_integration')]
     },
     {
       title: t('developer_frontend'),
       years: 'Aug 2023 - Oct 2023',
       place: 'AGCS LIDAL, Abomey-Calavi, Benin',
-      skills: [
-        t('fusion_sites'),
-        t('react_frontend'),
-      ]
+      skills: [t('fusion_sites'), t('react_frontend')]
     }
   ]
 }
